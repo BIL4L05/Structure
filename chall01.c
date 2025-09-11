6ac8f1eb858d38d8b4f0ca6a843c261e18092034
Your Name <bilalaitabdellah3@gmail.com>
2025-09-11 12:53:01 +0200

    Définition et Utilisation de Structure

---
+++ chall01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct personne {
+    char nom[50] , prenom[50];
+    int age ;
+};
+
+int main(){
+
+    struct personne p1 ;
+
+    printf("Taper votre prenom :");
+    scanf("%49s", &p1.nom);
+    printf("Taper votre nom :");
+    scanf("%49s", &p1.prenom);
+    printf("Taper votre age :");
+    scanf("%d", &p1.age);
+
+    printf("\n|| VOTRE INFORMATIONS CARD ||\n");
+    printf("Le nom : %s\n", p1.prenom);
+    printf("Le prenom : %s\n", p1.nom);
+    printf("L'age : %d ans\n", p1.age);
+
+    return 0 ;
+}
